8ebea73a4a7b43da2fb2d0aef1801f75b7dc97c1
Amelonworld <obumdavid@gmail.com>
2022-10-01 21:53:18 +0100

    final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,13 +1,17 @@
+from flask import Flask, render_template, request
 import json
-import os
-from ibm_watson import LanguageTranslatorV3
+# import os
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from dotenv import load_dotenv
+from ibm_watson import LanguageTranslatorV3
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+
+app = Flask("Web Translator")
+
+apikey = "Xa3MnyLFGi3PfYkMAJirHDC0B736-LeDgES7MOwXADLr"
+url = "https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/dedee887-4197-49cd-a125-bf7e5a87586f"
 
 authenticator = IAMAuthenticator(apikey)
 lt = LanguageTranslatorV3(
@@ -16,21 +20,28 @@ lt = LanguageTranslatorV3(
 )
 lt.set_service_url(url)
 
-
-def english_to_french(input):
-    translation = lt.translate(
-    text = input('Enter a text here: '),
-    model_id = 'en-fr').get_result()
-    french_text = json.dumps(translation, indent=2, ensure_ascii=False)
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('Hello, where are you going to')
+    translator = lt.translate(
+        text = textToTranslate,
+        model_id = 'en-fr').get_result()
+    french_text = json.dumps(translator, indent=2, ensure_ascii=False)
     return french_text
 
-english_to_french(input)
-
-def french_to_english(input):
-    translation = lt.translate(
-    text = input('Entrez un texte ici: '),
-    model_id = 'fr-en').get_result()
-    english_text = json.dumps(translation, indent=2, ensure_ascii=False)
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get("Bonjour, où vas-tu")
+    translator = lt.translate(
+        text = textToTranslate,
+        model_id = 'fr-en').get_result()
+    english_text = json.dumps(translator, indent=2, ensure_ascii=False)
     return english_text
 
-french_to_english(input)
+@app.route("/")
+def renderIndexPage():
+    # Write the code to render template
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
--- final_project/server.py
+++ final_project/server.py
@@ -1,31 +1,87 @@
-from machinetranslation import translator
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import json
+# import os
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import LanguageTranslatorV3
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
 
 app = Flask("Web Translator")
 
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('Hello, where are you going to')
-    translation = lt.translate(
-        text = textToTranslate,
-        model_id = 'en-fr').get_result()
-    french_text = json.dumps(translation, indent=2, ensure_ascii=False)
+apikey = "Xa3MnyLFGi3PfYkMAJirHDC0B736-LeDgES7MOwXADLr"
+url = "https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/dedee887-4197-49cd-a125-bf7e5a87586f"
+
+authenticator = IAMAuthenticator(apikey)
+lt = LanguageTranslatorV3(
+    version='2022-09-12',
+    authenticator=authenticator
+)
+lt.set_service_url(url)
+
+#@app.route("/englishToFrench")
+#def englishToFrench():
+    # textToTranslate = request.args.get()
+    #if request.method == "POST":
+        #textToTranslate = request.form["text"]
+        #translator = lt.translate(
+            #text = textToTranslate,
+            #model_id = 'en-fr').get_result()
+       # french_text = json.dumps(translator, indent=2, ensure_ascii=False)
+    #return french_text
+
+@app.route("/englishToFrench", methods=['POST', 'GET'])
+def nglishToFrench():
+    #textToTranslate = request.args.get(text)
+    if request.method == 'POST':
+        text = request.form.get('text')
+        translator = lt.translate(
+            text = text,
+            model_id = 'en-fr').get_result()
+        french_text = json.dumps(translator, indent=2, ensure_ascii=False)
     return french_text
+
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('Bonjour, où vas-tu')
-    translation = lt.translate(
-        text = textToTranslate,
+    #textToTranslate = request.args.get()
+    translator = lt.translate(
+        text = request.form.get('text'),
         model_id = 'fr-en').get_result()
-    english_text = json.dumps(translation, indent=2, ensure_ascii=False)
+    english_text = json.dumps(translator, indent=2, ensure_ascii=False)
     return english_text
 
-@app.route("/")
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        text = request.form.get('text')
+    return render_template('index.html', text=text)
+
+
+@app.route("/", methods=['POST', 'GET'])
 def renderIndexPage():
-    # Write the code to render template
-    return render_template('index.html')
+    def englishToFrench():
+    #textToTranslate = request.args.get(text)
+        if request.method == 'POST':
+            text = request.form.get('text')
+            translator = lt.translate(
+                text = text,
+                model_id = 'en-fr').get_result()
+            french_text = json.dumps(translator, indent=2, ensure_ascii=False)
+        return french_text
+    
+    def frenchToEnglish():
+        if request.method == 'POST':
+            text = request.form.get('text')
+    #textToTranslate = request.args.get()
+            translator = lt.translate(
+                text = text,
+                model_id = 'fr-en').get_result()
+            english_text = json.dumps(translator, indent=2, ensure_ascii=False)
+        return english_text
+    
+    return render_template('index.html', text=englishToFrench(), text2=frenchToEnglish())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
